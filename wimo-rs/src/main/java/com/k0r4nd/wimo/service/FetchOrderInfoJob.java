d2cac284286d2c7ee79da7e9413f0eb9f725a35c
git <mueller.marcel2@gmail.com>
2016-02-07 00:49:28 +0100

    fixed NPE during first orderInfo job execution

--- wimo-rs/src/main/java/com/k0r4nd/wimo/service/FetchOrderInfoJob.java
+++ wimo-rs/src/main/java/com/k0r4nd/wimo/service/FetchOrderInfoJob.java
@@ -1,5 +1,7 @@
 package com.k0r4nd.wimo.service;
 
+import java.util.Date;
+
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
@@ -25,9 +27,14 @@ public class FetchOrderInfoJob implements Job {
 		Order order = (Order) jobInfos.get("order");
 
 		Order newOrder = service.findByTrackingId(order.getTrackingId());
-		Long lastExecution = context.getPreviousFireTime().getTime();
+		// for first schedule use current time
+		Long lastExecution = new Date().getTime();
+		// if job was fired before use previous job fireTime
+		if (context.getPreviousFireTime() != null) {
+			lastExecution = context.getPreviousFireTime().getTime();
+		}
 		if (order.getLastStatusUpdate() > lastExecution) {
-			System.out.println("pushing Order " +order.getId() );
+			System.out.println("pushing Order " + order.getId());
 			newOrder.setId(order.getId());
 			orderService.save(newOrder);
 			pushService.pushOrderUpdate(newOrder);
